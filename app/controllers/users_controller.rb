05e2ce8cd996394ffa7cc726ffd2c04a21e6a848
kush Muhamad <kushmohammed98@yahoo.com>
2023-07-01 17:54:05 +0300

    completed task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+  before_action :authorize, only: [:show, :index]
 
   def show
-    user = User.find(session[:user_id])
-    render json: user
+    user = User.find_by(id: session[:user_id])
+
+    if user
+      render json: user
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
   end
 
+  def index
+    users = User.all
+    render json: users
+  end
+
+  private
+
+  def authorize
+    return render json: { error: 'Not authorized' }, status: :unauthorized unless session[:user_id].present?
+  end
 end
